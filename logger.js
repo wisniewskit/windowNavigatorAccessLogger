0b6f624fa67f9cd4f5333846e80e2f622ead2a70
Thomas Wisniewski <wisniewskit@gmail.com>
2017-05-09 18:19:17 -0400

    Also log XHR.send() and fetch() calls.

--- logger.js
+++ logger.js
@@ -6,23 +6,47 @@ let I18NMessage = browser.i18n.getMessage("consoleMessage");
 // so we can override the wrapped window's properties here.
 window.wrappedJSObject.eval(`(function(I18NMessage) {
   let origConsole = console;
+
+  function trace(obj, stripFirst=2) {
+    // console.trace doesn't get the full trace here, so we
+    // force-throw an exception manually and log it.
+    try {
+      let i = Proxy(); // Guaranteed to throw per-spec (needs "new").
+    } catch(_) {
+      origConsole.error(I18NMessage.replace("OBJECT", obj) + "\\n" +
+                        _.stack.split("\\n").splice(stripFirst).join("\\n"));
+    }
+  }
+
   for (let key in navigator) {
     let origValue = navigator[key];
     Object.defineProperty(navigator, key, {
       get: function() {
-        // console.trace doesn't get the full trace here, so we
-        // force-throw an exception manually and log it.
-        try {
-          let i = Proxy(); // Guaranteed to throw per-spec (needs "new").
-        } catch(_) {
-          origConsole.error(I18NMessage.replace("KEY", key) + "\\n" +
-                            _.stack.split("\\n").splice(1).join("\\n"));
-        }
+        trace("navigator." + key);
         return origValue.constructor === Function ?
                  origValue.call(null, arguments) : origValue;
       },
       configurable: true // So reloading the addon doesn't throw an error.
     });
   }
+
+  function overrideFn(obj, name, replacement) {
+    let original = obj[name];
+    replacement.toString = original.toString;
+    replacement.toSource = original.toSource;
+    obj[name] = function() {
+      replacement.call(this, arguments);
+      return original.call(this, arguments);
+    }
+    return replacement;
+  }
+
+  overrideFn(XMLHttpRequest.prototype, "send", function() {
+    trace("XMLHttpRequest.send", 3);
+  });
+
+  overrideFn(window, "fetch", function() {
+    trace("fetch", 3);
+  });
 }("${I18NMessage}"));`);
 
